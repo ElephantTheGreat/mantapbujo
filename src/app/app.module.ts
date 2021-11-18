6c10e5b4d748cf25f941451aed0c35e6688b85a9
manabotcard <manabot.cards@gmail.com>
2021-11-18 08:04:25 +0700

    menambahkan component card-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
